cf8065615651d3851ca93ee08f1cd67112cd9282
Null McNull <no@spam.org>
2014-09-03 09:17:46 +0200

    dist build

--- dist/angular-block-ui.js
+++ dist/angular-block-ui.js
@@ -1,5 +1,5 @@
 /*!
-   angular-block-ui v0.0.12
+   angular-block-ui v0.1.0-beta.1
    (c) 2014 (null) McNull https://github.com/McNull/angular-block-ui
    License: MIT
 */
@@ -222,76 +222,6 @@ blkUI.constant('blockUIConfig', {
     cssClass: 'block-ui block-ui-anim-fade'
 });
 
-//  this.templateUrl = function (url) {
-//    _config.templateUrl = url;
-//  };
-//
-//  this.template = function (template) {
-//    _config.template = template;
-//  };
-//
-//  this.delay = function (delay) {
-//    _config.delay = delay;
-//  };
-//
-//  this.message = function (message) {
-//    _config.message = message;
-//  };
-//
-//  this.autoBlock = function (enabled) {
-//    _config.autoBlock = enabled;
-//  };
-//
-//  this.resetOnException = function (enabled) {
-//    _config.resetOnException = enabled;
-//  };
-//
-//  this.requestFilter = function (filter) {
-//    _config.requestFilter = filter;
-//  };
-//
-//  this.autoInjectBodyBlock = function (enabled) {
-//    _config.autoInjectBodyBlock = enabled;
-//  };
-//
-//  function _cssClassArray(target) {
-//    return function(classes, replace) {
-//      if (classes !== undefined) {
-//
-//        if (angular.isString(classes)) {
-//          classes = classes.split(' ');
-//        }
-//
-//        if (replace) {
-//          _config[target] = classes;
-//        } else {
-//          var i = classes.length;
-//
-//          while (i--) {
-//            _config[target].push(classes[i]);
-//          }
-//        }
-//      }
-//    }
-//  }
-//
-//  this.cssClass = _cssClassArray('cssClass');
-//  this.cssClassMessage = _cssClassArray('cssClassMessage');
-//
-//  this.$get = ['$templateCache', function ($templateCache) {
-//
-//    if (_config.template) {
-//
-//      // Swap the builtin template with the custom template.
-//      // Create a unique cache key and place the template in the cache.
-//
-//      _config.templateUrl = '$$block-ui-template$$';
-//      $templateCache.put(_config.templateUrl, _config.template);
-//    }
-//
-//    return _config;
-//  }];
-//});
 
 blkUI.factory('blockUIHttpInterceptor', ["$q", "$injector", "blockUIConfig", function($q, $injector, blockUIConfig) {
 

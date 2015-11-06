1bc6e1760871c0cd2a1a27b2fa1c47a1026d3d13
Null McNull <nospam@nowhere.com>
2015-11-06 15:47:05 +0100

    animations demo

--- src/sandbox/block-ui-animate/script.js
+++ src/sandbox/block-ui-animate/script.js
@@ -3,29 +3,38 @@ var app = angular.module('myApp', ['blockUI']);
 
 app.directive('blockUiAnimate', function ($timeout) {
 
-  function getChild(parent, selector) {
-    var child, needsId = !parent.id;
-
-    if (needsId) {
-      parent.id = 'TEMPID____' + (new Date()).getTime();
-    }
-
-    child = document.querySelector('#' + parent.id + ' ' + selector);
-
-    if (needsId) {
-      parent.id = '';
-    }
-
-    return child;
-  }
-
+  var startEvent = 'block-ui-visible-start';
+  var visibleLoop = 'block-ui-visible-loop';
+  var endEvent = 'block-ui-visible-end';
+  var animationEndEvent = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+  
   return {
     priority: -100,
     restrict: 'C',
     link: function ($scope, $element, $attrs) {   
-      var $container = getChild($element[0], '.block-ui-container');
       var instance = $element.data('block-ui');
       
+      $scope.$on(startEvent, function(e, args) {
+        if(args.instance === instance) {
+          $element.addClass(startEvent);
+          
+          $element.one(animationEndEvent, function() {
+            $element.addClass(visibleLoop);
+            $element.removeClass(startEvent);
+          });     
+        }
+      });
+      
+      $scope.$on(endEvent, function(e, args) {
+        if(args.instance === instance) {
+          $element.removeClass(visibleLoop);
+          $element.addClass(endEvent);
+          
+          $element.one(animationEndEvent, function() {
+            $element.removeClass(endEvent);
+          });     
+        }
+      });
     }
   };
 });

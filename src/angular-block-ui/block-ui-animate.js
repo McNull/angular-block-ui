9bfd2a4901826adaaf8e77f65d9205d101435dfb
Null McNull <nospam@nowhere.com>
2015-11-06 16:38:38 +0100

    animations

---
+++ src/angular-block-ui/block-ui-animate.js
@@ -0,0 +1,41 @@
+blkUI.directive('blockUiAnimate', function () {
+
+  var startEvent = 'block-ui-visible-start';
+  var visibleLoop = 'block-ui-visible-loop';
+  var endEvent = 'block-ui-visible-end';
+  var animationEndEvent = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+  
+  return {
+    priority: -100,
+    restrict: 'C',
+    link: function ($scope, $element, $attrs) {   
+      var instance = $element.data('block-ui');
+      
+      $scope.$on(startEvent, function(e, args) {
+        if(args.instance === instance) {
+          $element.addClass(startEvent);
+          
+          $element.one(animationEndEvent, function() {
+            
+            if(instance.isBlocking()) {
+              $element.addClass(visibleLoop);  
+            }
+            
+            $element.removeClass(startEvent);
+          });     
+        }
+      });
+      
+      $scope.$on(endEvent, function(e, args) {
+        if(args.instance === instance) {
+          $element.removeClass(visibleLoop);
+          $element.addClass(endEvent);
+          
+          $element.one(animationEndEvent, function() {
+            $element.removeClass(endEvent);
+          });     
+        }
+      });
+    }
+  };
+});
